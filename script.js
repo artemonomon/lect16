f8ab799f07b895b5afe0b3de4ec1a8a21e2e14ca
artemonomon <65706452+artemonomon@users.noreply.github.com>
2022-02-01 13:23:00 +0200

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+let table = document.getElementById("age-table");
+
+let labels = table.getElementsByTagName("label");
+
+let firstTd = table.querySelector("td");
+
+let formName = document.getElementsByName("search")[0];
+
+let firstInput = formName.querySelector("input");
+
+let inputs = form.querySelectorAll("input"); 
+let lastInput = inputs[inputs.length-1];
+
